40d6c0f4fbe3dec4f958e26f0c8981bcaca69def
Alan Turing <freemasl.sf@gmail.com>
2019-09-18 11:10:24 -0600

    Add createUser action creator test

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,6 +1,23 @@
 import * as actions from './index';
 
 describe('actions', () => {
+  it('should have a type of CREATE_USER,', () => {
+    const mockUser = {
+      id: 42,
+      firstName: 'Sam',
+      lastName: 'Freeman',
+      feeling: 'happy',
+    }
+    const expectedAction = {
+      type: 'CREATE_USER',
+      user: mockUser
+    }
+
+    const result = actions.createUser(mockUser);
+
+    expect(result).toEqual(expectedAction);
+  });
+
   it('should have a type of ADD_MESSAGE,', () => {
     const mockMessage = 'Hey Travis and Robbie';
     const mockisUser = true;
@@ -13,7 +30,7 @@ describe('actions', () => {
     const result = actions.addMessage(mockMessage, mockisUser);
 
     expect(result).toEqual(expectedAction);
-  })
+  });
 
   it('should have a type of CLEAR_MESSAGES,', () => {
     const expectedAction = {
@@ -23,5 +40,5 @@ describe('actions', () => {
     const result = actions.clearMessages();
 
     expect(result).toEqual(expectedAction);
-  })
+  });
 })
