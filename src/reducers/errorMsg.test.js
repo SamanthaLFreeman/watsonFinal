7a741bfd0d50eeb051ef332bc64331c6d288b3a2
Alan Turing <freemasl.sf@gmail.com>
2019-09-18 11:22:01 -0600

    Add test for errorMsg reducer when HAS_ERRORED action is passed through

--- src/reducers/errorMsg.test.js
+++ src/reducers/errorMsg.test.js
@@ -8,4 +8,14 @@ describe('errorMsg reducer', () => {
     expect(result).toEqual(expected);
   });
 
+  it('should return error message when HAS_ERRORED action is passed through', () => {
+    const expected = 'There\'s been an error!';
+    const actionObject = {
+      type: 'HAS_ERRORED',
+      errorMsg: 'There\'s been an error!'
+    }
+    const result = errorMsg(undefined, actionObject);
+
+    expect(result).toEqual(expected);
+  });
 })
